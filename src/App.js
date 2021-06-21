c25e1e9010fd66063cdbb3a0cdf85fedbf8f9c6c
Dreadnought1 <j.a.vermeulen1999@hotmail.com>
2021-06-21 13:39:31 +0200

    subsection-107

--- src/App.js
+++ src/App.js
@@ -22,19 +22,14 @@ class App extends React.Component {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await CreateUserProfileDocument(userAuth);
-        userRef.onSnapshot(
-          (snapShot) => {
-            this.setState({
-              currentUser: {
-                id: snapShot.id,
-                ...snapShot.data(),
-              },
-            });
-          },
-          () => {
-            console.log(this.state);
-          }
-        );
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
       } else {
         this.setState({ currentUser: userAuth });
       }
